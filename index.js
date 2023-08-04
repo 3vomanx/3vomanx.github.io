c59003a713c4557f32bb1b941ef4029bf6628464
3vomanx <giorgigogidze39@gmail.com>
2023-08-04 23:57:50 +0400

    .

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ var express = require("express"),
     temp_partner,
     num_users=0;
 
-app.use('/static',express.static(__dirname+"/static"));
+    app.use(express.static(distDir));
+
+
 // app.use('/emoji/images',express.static(__dirname+"/node_modules/emoji-parser/emoji/"));
 
 app.get('/', function(req, res){
